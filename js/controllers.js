d71c223830d85045e04c039d25688161867b0e97
Vladimir Lelicanin <v.lelicanin@sae.edu>
2015-05-14 16:48:42 +0200

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,47 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Morning', id: 1, color: 'red' },
+    { title: 'Dance', id: 2, color: 'green' },
+    { title: 'Gypsies', id: 3 },
+    { title: 'Wedding', id: 4 },
+    { title: 'Obilic', id: 5 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
